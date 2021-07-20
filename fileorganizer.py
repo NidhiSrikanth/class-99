49ba886f803baec64eef39ec88160475ffd405c4
NidhiSrikanth <65636801+NidhiSrikanth@users.noreply.github.com>
2021-07-20 12:59:56 -0400

    Add files via upload

---
+++ fileorganizer.py
@@ -0,0 +1,19 @@
+import os 
+import shutil
+
+path= input("enter the name of the directory to be sorted")
+listoffiles= os.listdir(path)
+
+for file in listoffiles:
+    name, ext= os.path.splitext(file)
+    ext= ext [1:]
+
+    if ext==" ":
+        continue
+
+    if os.path.exists(path+"/"+ext):
+        shutil.move(path+"/"+file,path+"/"+ext+"/"+file)
+
+    else:
+        os.mkdir(path+"/"+ext)
+        shutil.move(path+"/"+file,path+"/"+ext+"/"+file)
